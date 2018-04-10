0dd7952ee20559e05dbca87109bb58c85e6d638d
Andrey <staybox@gmail.com>
2018-04-10 14:46:02 +0300

    finish

--- index.php
+++ index.php
@@ -4,15 +4,6 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-/*
-// Подключение файлов
-define('ROOT', dirname(__FILE__));
-require_once (ROOT.'/components/router.php');
-
-$router = new Router();
-$router->run();
-*/
-
 // начинаем работать с сессией
 session_start();
 
